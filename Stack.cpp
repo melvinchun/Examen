9c6c3447193029f40b52f6c5ad8212ec8fdcfe3a
Isaias Valle <guitartsword@gmail.com>
2015-06-26 13:46:14 -0600

    Constructor destructor y el push

--- Stack.cpp
+++ Stack.cpp
@@ -2,16 +2,23 @@
 
 
 Stack::Stack(){
-
+	size = 1;
+	array = new int[size];
 }
 Stack::Stack(const Stack&){
 
 }
 Stack::~Stack(){
-
+	delete[] array;
 }
-void Stack::push(int){
-
+void Stack::push(int newNumber){
+	int* stackTemp = new int[++size];
+	for(int i = 0; i < size-1 ; i++){
+		stackTemp[i] = array[i];
+	}
+	stackTemp[size-1] = newNumber;
+	delete[] array;
+	array = stackTemp;
 }
 int Stack::pop(){
 
